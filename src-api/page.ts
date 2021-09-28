f50bbc646bc4e556a4f325300a6b320104bfd7c8
Lennard Sprong <x-sheep@users.noreply.github.com>
2021-09-28 19:04:49 +0200

    Implement sendPage as serverless function

---
+++ src-api/page.ts
@@ -0,0 +1,65 @@
+import fs = require('fs');
+import { VercelResponse, VercelRequestQuery } from '@vercel/node';
+import { parse_query, pzvdetails } from "./tools"
+
+const pzprdir = process.env.PZPR_DIR || 'dist';
+const templates = process.env.TEMPLATE_DIR || 'src-api/templates';
+
+const rawpage = fs.readFileSync(pzprdir + '/p.html', 'utf8');
+const parts = rawpage.split(/<title>[^<]*<\/title>/i);
+const head = parts[0];
+const body = parts[1];
+const metatmpl = fs.readFileSync(templates + '/meta.template', 'utf8');
+const callbacktmpl = fs.readFileSync(templates + '/callback.template', 'utf8');
+
+function substitute(tmpl: string, vars: Record<string, string>): string {
+	for (var key in vars) {
+		tmpl = tmpl.replace(new RegExp('%%' + key + '%%', 'g'), vars[key]);
+	}
+	return tmpl;
+}
+
+export function sendPage(res: VercelResponse, query: VercelRequestQuery) {
+	var qargs = parse_query(query);
+
+	if (!qargs.pzv) {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.write(head);
+		res.write(substitute(callbacktmpl, {}));
+		res.end(body);
+		return;
+	}
+	try {
+		const p = pzvdetails(qargs.pzv);
+		var size = "";
+		if (!isNaN(p.cols) && !isNaN(p.rows)) {
+			size = "" + p.rows + "×" + p.cols;
+		}
+		var title = p.title;
+		var desc = 'Solve a ' + p.title + ' puzzle';
+		if (size) {
+			title = size + ' ' + title;
+			desc += ', size ' + size;
+		}
+		desc += '.';
+		var vars: Record<string, string> = {
+			'CANONICAL_URL': 'https://puzz.link/p?' + qargs.pzv,
+			'TITLE': title,
+			'DESCRIPTION': desc,
+			'PREVIEW_IMG': 'https://puzz.link/pv?frame=5&' + qargs.pzv,
+			'PZV': qargs.pzv
+		};
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.write(head);
+		res.write(substitute(metatmpl, vars));
+		res.write(substitute(callbacktmpl, vars));
+		res.end(body);
+	} catch(err) {
+		console.log('caught error', err, 'sending raw page');
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(rawpage);
+	}
+}
---
+++ src-api/tools.ts
@@ -0,0 +1,42 @@
+import { VercelRequestQuery } from "@vercel/node";
+import pzpr from "../dist/js/pzpr.js"
+
+export function parse_query(query: VercelRequestQuery) {
+	var args = {
+		thumb: false,
+		frame: 0,
+		svgout: false,
+		pzv: '',
+	};
+
+	for (var [part, value] of new Map(Object.entries(query))) {
+		if (part === 'thumb') {
+			args.thumb = true;
+		} else if (part === 'svg') {
+			args.svgout = true;
+		} else if (part === 'frame') {
+			args.frame = +value;
+		} else if (args.pzv === '' && part.match(/^[\w-]+\//)) {
+			args.pzv = part;
+		}
+	}
+	return args;
+}
+
+export interface PuzzleDetails {
+	pid: string;
+	title: string;
+	cols: number;
+	rows: number;
+}
+
+export function pzvdetails(pzv: string): PuzzleDetails {
+	const urldata = pzpr.parser.parseURL(pzv);
+	const info = pzpr.variety(urldata.pid);
+	return {
+		pid: urldata.pid,
+		title: info.en,
+		cols: urldata.cols,
+		rows: urldata.rows
+	}
+}
